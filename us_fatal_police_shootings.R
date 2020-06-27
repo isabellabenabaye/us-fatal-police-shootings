1ebf1bba3e3815d0735b6622bcd0c21d406c560a
Isabella Benabaye <isabellabenabaye@gmail.com>
2020-06-26 19:37:36 -0700

    :tada: cities table

--- us_fatal_police_shootings.R
+++ us_fatal_police_shootings.R
@@ -4,6 +4,7 @@ library(reactable)
 
 fatal_police_shootings <- read_csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv")
 
+# States table -----
 # state counts pre-pivot - used for cell shading
 state_counts <- fatal_police_shootings %>%
   count(state, race, sort = TRUE)
@@ -61,11 +62,16 @@ reactable(
   data = state_table,
   style = list(fontFamily = "Work Sans, sans-serif", fontSize = "16px"),
   defaultSorted = "total",
+  defaultPageSize = 25,
+  highlight = TRUE,
+  searchable = TRUE,
+  compact = TRUE,
   columnGroups = list(
     colGroup(name = "Race", columns = race_cols)
   ),
   columns = list(
-    state_name = colDef(name = "State"),
+    state_name = colDef(name = "State",
+                        minWidth = 120),
     total = colDef(
       name = "Number of killings",
       defaultSortOrder = "desc",
@@ -90,3 +96,85 @@ reactable(
     unknown = race_column(name = "Unknown", col = "unknown")
   )
 )
+
+
+# Cities table -----
+# city counts pre-pivot - used for cell shading
+city_counts <- fatal_police_shootings %>%
+  mutate(city = paste0(city,", ",state)) %>%
+  count(city, race, sort = TRUE)
+
+# main table
+city_table <- fatal_police_shootings %>%
+  mutate(city = paste0(city,", ",state)) %>%
+  count(city, race) %>%
+  ## make a column for each race
+  pivot_wider(id_cols = city, names_from = race, values_from = n, values_fill = 0) %>%
+  rename(unknown = `NA`) %>%
+  mutate(total = A + B + N + W + unknown + H + O) %>%
+  select(city, total, W, B, H, A, N, O, unknown) %>%
+  filter(total >= 10)
+
+# race column
+race_column <- function(col, class = NULL, ...){
+  colDef(
+    ...,
+    maxWidth = 80,
+    defaultSortOrder = "desc",
+    headerStyle = list(fontSize = "13px", fontWeight = 400),
+    style = function(value) {
+      normalized <- (value - min(city_counts[-1:-2,"n"])) / (max(city_counts[-1:-2,"n"]) - min(city_counts[-1:-2,"n"]))
+      if (value < 1) {
+        list(color = "#aaa")
+      } else if (normalized > 0.99) {
+        list(background = "#f87274")
+      } else {
+        list(background = red_pal(normalized))
+      }
+    }
+  )
+}
+
+# reactable table
+final_table_cities <- reactable(
+  data = city_table,
+  style = list(fontFamily = "Work Sans, sans-serif", fontSize = "14px", background = "#F7F7F7"),
+  defaultSorted = "total",
+  defaultPageSize = 15,
+  defaultColDef = colDef(class = "cell", headerClass = "header"),
+  highlight = TRUE,
+  searchable = TRUE,
+  compact = TRUE,
+  columnGroups = list(
+    colGroup(name = "Race", columns = race_cols)
+  ),
+  columns = list(
+    city = colDef(name = "State",
+                  minWidth = 120,
+                  style = list(fontWeight = 500)),
+    total = colDef(
+      name = "Number of killings",
+      defaultSortOrder = "desc",
+      minWidth = 160,
+      cell = function(value) {
+        width <- paste0(value * 100 / max(city_table$total), "%")
+        value <- format(value, big.mark = ",")
+        # Fix each label using the width of the widest number (incl. thousands separators)
+        value <- format(value, width = 4, justify = "right")
+        bar_chart(value, width = width, fill = "#3fc1c9")
+      },
+      # And left-align the columns
+      align = "left",
+      style = list(fontFamily = "DM Mono", whiteSpace = "pre")
+    ),
+    W = race_column(name = "White", col = "W"),
+    B = race_column(name = "Black", col = "B"),
+    H = race_column(name = "Hispanic", col = "H"),
+    A = race_column(name = "Asian", col = "A"),
+    N = race_column(name = "Native American", col = "N"),
+    O = race_column(name = "Other", col = "O"),
+    unknown = race_column(name = "Unknown", col = "unknown")
+  )
+)
+
+final_table_cities
